af84acd8862adf625cbd20a80e0ad4d0f676596e
Willian Oliveira <williangd@yahoo.com.br>
2017-11-06 14:42:20 -0200

    Add riquiredAuth

---
+++ client/components/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Dashboard = props => (
+  <div>
+    Dashboard
+  </div>
+);
+
+export default Dashboard;
--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -3,12 +3,19 @@ import { graphql } from 'react-apollo';
 import AuthForm from './AuthForm';
 import mutation from '../mutations/Login';
 import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
 
 class LoginForm extends Component {
   constructor(props) {
     super(props)
 
     this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard');
+    }
   }
 
   onSubmit({ email, password }) {
@@ -36,4 +43,6 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(query)(
+graphql(mutation)(LoginForm)
+);
